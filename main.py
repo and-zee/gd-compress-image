8aadab02980e68641b283fb5f695eae6ece46953
“Andre <hello.andre@workmail.com>
2022-12-03 10:01:18 +0000

    add replace file

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def compressImg(src_file, destpath, filename):
     new_img_size = os.path.getsize(file_compressed)
     return img_size, new_img_size
 
-def compressReplaceImg(src_file, filename):
+def compressReplaceImg(src_file):
     picture = Image.open(src_file)
     img_size = os.path.getsize(src_file)
         
@@ -93,7 +93,7 @@ def main(_basepath, only=False, onlyreplace=False, replace=False):
             filepath=str(_basepath)+"/"+file
             if os.path.splitext(file)[1].lower() in formats:
                 logger.info("Compressing "+str(file))
-                img_size, new_img_size = compressReplaceImg(filepath, file)
+                img_size, new_img_size = compressReplaceImg(filepath)
                 saving_diff = new_img_size - img_size
                 logger.info("[+] Original image size: {}".format(convert_size(img_size)))
                 logger.info("[+] Compressed image size: {}".format(convert_size(new_img_size)))
@@ -102,20 +102,66 @@ def main(_basepath, only=False, onlyreplace=False, replace=False):
         logger.info("Done")
         elapsed_time = time.time() - st
         logger.info("Execution time : "+str(time.strftime("%H:%M:%S", time.gmtime(elapsed_time))))
-    elif replace: print(replace)
+    elif replace: 
+        formats = ('.jpg', '.jpeg')
+        dotenv_path = Path(str(os.environ["env"]))
+        load_dotenv(dotenv_path=dotenv_path)
+        base_dir=str(os.getenv('SOURCE_DIR'))
+        
+        # Loop though all account
+        for account in os.listdir(str(base_dir)):
+            # Loop through all years and month
+            account_dir=base_dir+"/"+str(account)
+            
+            log_file = Path(str(account_dir)+'/_process.log')
+            if not os.path.exists(log_file): log_file.touch(exist_ok=True)
+            
+            logger = logging.getLogger()
+            logger.setLevel(logging.NOTSET)
+            log_handler = logging.FileHandler(log_file)
+            log_handler.setLevel(logging.INFO)
+            log_handler_format = '%(asctime)s : %(message)s'
+            log_handler.setFormatter(logging.Formatter(log_handler_format))
+            logger.addHandler(log_handler)
+            
+            for month in os.listdir(str(account_dir)):
+                # Loop through all days
+                days = str(account_dir)+"/"+month
+                for day in os.listdir(days):
+                    # Loop through all hours
+                    hours = str(days)+"/"+day
+                    for hour in os.listdir(hours):
+                        # Loop through all files
+                        src_dir=str(hours)+"/"+hour
+                        for file in os.listdir(src_dir):
+                            filepath=str(src_dir)+"/"+file
+                            if os.path.splitext(file)[1].lower() in formats:
+                                logger.info("Compressing "+str(file))
+                                img_size, new_img_size = compressReplaceImg(filepath)
+                                saving_diff = new_img_size - img_size
+                                logger.info("[+] Original image size: {}".format(convert_size(img_size)))
+                                logger.info("[+] Compressed image size: {}".format(convert_size(new_img_size)))
+                                logger.info(f"[+] Image size change: {saving_diff/img_size*100:.2f}% of the original image size.")
+                            else: logger.error("Unsupported file format")
+            logger.info("Compressed directory : {}".format(account_dir))
+        logger.info("Done")
+        elapsed_time = time.time() - st
+        logger.info("Execution time : "+str(time.strftime("%H:%M:%S", time.gmtime(elapsed_time))))
+
+        
     else:
         formats = ('.jpg', '.jpeg')
         dotenv_path = Path(str(os.environ["env"]))
         load_dotenv(dotenv_path=dotenv_path)
         base_dir=str(os.getenv('SOURCE_DIR'))
         dest_dir=str(os.getenv('DESTINATION_DIR'))
-        if not dest_dir:
+        if not dest_dir: # Variable defined but with no value
             parent_dir=os.path.abspath(os.path.join(base_dir, os.pardir))
             dest_dir=parent_dir+"/compressed"
-        elif dest_dir == "None":
+        elif dest_dir == "None": # No variable defined on .env
             parent_dir=os.path.abspath(os.path.join(base_dir, os.pardir))
             dest_dir=parent_dir+"/compressed"
-        else: 
+        else:  # Variable defined with its value
             if not os.path.exists(dest_dir): os.mkdir(dest_dir)
         
         # Loop though all account
@@ -171,7 +217,6 @@ def main(_basepath, only=False, onlyreplace=False, replace=False):
 def checkParam():
     parser = argparse.ArgumentParser()
     subparser = parser.add_subparsers(dest='command')
-    parser.add_argument('--replace', default=False, action='store_true')
     only = subparser.add_parser('only')
     onlyreplace = subparser.add_parser('onlyreplace')
     only.add_argument('--path', type=str, required=True)
@@ -181,18 +226,23 @@ def checkParam():
     basepath=""
     only=False
     onlyreplace=False
-    replace=False
     if args.command == 'only':
         only=True
         basepath=args.path
     elif args.command == 'onlyreplace':
         onlyreplace=True
         basepath=args.path
-    elif args.replace: replace= True
     
-    return only, basepath, replace, onlyreplace
+    return only, basepath, onlyreplace
 
 # Driver code
 if __name__ == "__main__":
-    _only, _basepath, _replace, _onlyreplace = checkParam()
+    dotenv_path = Path(str(os.environ["env"]))
+    load_dotenv(dotenv_path=dotenv_path)
+    is_replace=str(os.getenv('REPLACE'))
+    if is_replace: _replace=is_replace          # Variable defined with its value
+    elif is_replace == "None": _replace=False   # No variable defined on .env
+    else: _replace=False                        # Variable defined but with no value or with value False
+    
+    _only, _basepath, _onlyreplace = checkParam()
     main(_basepath=str(_basepath), only=_only, onlyreplace=_onlyreplace, replace=_replace)
